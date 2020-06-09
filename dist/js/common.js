2704cebdb34b8788fb7b70eea87eed581fd41afe
GuldauGuldau <32545778+GuldauGuldau@users.noreply.github.com>
2020-06-09 14:03:00 +0300

    Add files via upload

---
+++ dist/js/common.js
@@ -0,0 +1,59 @@
+var localUserStatus = "изменить статус";
+var inputStatus = userstatusform.querySelector('[name="status"]');
+var elemInfoStatus = document.querySelector(".info-status");
+
+// Show/Hide User Menu ----------------------------------------------------
+showmenu.addEventListener( "click" , function() {
+  document.querySelector(".user").classList.toggle("open-menu");
+});
+
+// Set User Status---------------------------------------------------------
+if(localStorage.userstatus) {
+  localUserStatus = localStorage.userstatus;
+}
+userstatus.innerText = inputStatus.value = localUserStatus;
+
+// Change User Status-------------------------------------------------------
+userstatus.addEventListener( "click" , function() {
+  elemInfoStatus.classList.toggle("open-form");
+});
+userstatusform.addEventListener( "submit" , function(e) {
+  e.preventDefault();
+  localStorage.userstatus = userstatus.innerText = inputStatus.value;
+  elemInfoStatus.classList.remove("open-form");
+});
+
+// Load Comments------------------------------------------------------------
+axios.get('php/ajax.php')
+  .then(function (response) {
+    document.querySelector(".comments-list").innerHTML = response.data;
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+
+// Add Comment-------------------------------------------------------------
+commentform.addEventListener( "submit" , function(e) {
+  e.preventDefault();
+  var params = new URLSearchParams();
+  var inputName = commentform.querySelector("[name='name']");
+  var inputMsg = commentform.querySelector("[name='message']");
+  params.append('name', inputName.value);
+  params.append('message', inputMsg.value);
+  params.append('avatar', 'img/logan.png');
+  params.append('action', 'addComment');
+  axios.post('php/ajax.php', params).then(function(response) {
+      document.querySelector(".comments-list").innerHTML = response.data;
+      commentform.reset();
+  });
+});
+
+// Delete Comment------------------------------------------------------------
+function deleteComment(id) {
+  var params = new URLSearchParams();
+  params.append('comment', id);
+  params.append('action', 'deleteComment');
+  axios.post('php/ajax.php', params).then(function(response) {
+      document.querySelector(".comments-list").innerHTML = response.data;
+  });
+}
---
+++ dist/php/ajax.php
@@ -0,0 +1,44 @@
+<?
+// Connect DB
+try {
+  $db = new PDO("sqlite:../electron.sqlite");
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+// Actions for comments
+if(isset($_POST['action'])) {
+  $action = $_POST['action'];
+  switch ($action) {
+    case 'addComment':
+        $date = new DateTime();
+        $data = array( 'name' => $_POST['name'], 'message' => $_POST['message'], 'avatar' => $_POST['avatar'], 'created_at' => $date->getTimestamp() );
+        $query = $db->prepare('INSERT INTO comments(name, message, avatar, created_at) VALUES(:name, :message, :avatar, :created_at)');
+        $query->execute($data);
+        break;
+    case 'deleteComment':
+        $commentId = $_POST['comment'];
+        $db->exec('delete from comments WHERE id="' . $commentId . '"');
+        break;
+    }
+}
+
+// Get Comments
+$results = $db->query('SELECT * FROM comments');
+$results->setFetchMode(PDO::FETCH_ASSOC);
+$comments = '';
+foreach($results->fetchAll() as $res) {
+  $comments .= '<div class="comment box">
+            <header class="comment-head">
+              <div class="comment-avatar"><img src="' . $res['avatar'] . '" alt="comment author"></div>
+              <div class="comment-author">
+                <h3 class="comment-author-name">' . $res['name'] . '</h3>
+                <div class="comment-time">вчера в ' . date('h:i', $res['created_at']) . '</div>
+              </div>
+            </header>
+            <div class="comment-content">' . $res['message'] . '</div>
+            <div class="comment-delete" data-id="' . $res['id'] . '" onclick="deleteComment(' . $res['id'] . ')"><img src="img/svg/delete.svg"></div>
+          </div>';
+}
+echo $comments;
